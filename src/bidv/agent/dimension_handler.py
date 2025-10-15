7410b14c4765d0df94cf0d03da38774585e0e8d6
ptanX <nguyenthaibinhbk@gmail.com>
2025-10-15 08:04:02 +0700

    commit code for prompt of bidv lending

---
+++ src/bidv/agent/dimension_handler.py
@@ -0,0 +1,29 @@
+from typing import Dict
+
+from src.graph.node_executions_dispatcher import ExecutionHandler
+
+
+class CapitalHandler(ExecutionHandler[Dict]):
+
+    async def handle(self, execution_input: Dict) -> Dict:
+        return {}
+
+
+class AssetHandler(ExecutionHandler):
+    async def handle(self, execution_input: Dict) -> Dict:
+        return {}
+
+
+class ManagementHandler(ExecutionHandler):
+    async def handle(self, execution_input: Dict) -> Dict:
+        return {}
+
+
+class EarningHandler(ExecutionHandler):
+    async def handle(self, execution_input: Dict) -> Dict:
+        return {}
+
+
+class LiquidityHandler(ExecutionHandler):
+    async def handle(self, execution_input: Dict) -> Dict:
+        return {}
